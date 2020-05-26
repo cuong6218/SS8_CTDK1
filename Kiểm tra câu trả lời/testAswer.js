7c13fc581e97182c8bd0649b8dfda81cdd7576c4
Rea Sakyo <cuong6218@gmail.com>
2020-05-26 13:45:21 +0700

    them file kiem tra cau tra loi

---
+++ Kiểm tra câu trả lời/testAswer.js
@@ -0,0 +1,7 @@
+function testAnswer(){
+    let a = document.getElementById("textbox").value;
+    if (a=="ECMAScript"){
+        alert("Right!");
+    }
+    else alert("Didn't know? ECMAScript!");
+}
