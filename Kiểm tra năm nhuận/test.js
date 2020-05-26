5680af17987036efbab807f01613dc45eba141bc
Rea Sakyo <cuong6218@gmail.com>
2020-05-26 13:32:07 +0700

    cap nhat file kiem tra nam nhuan

--- Kiểm tra năm nhuận/test.js
+++ Kiểm tra năm nhuận/test.js
@@ -1,7 +1,14 @@
 function kiemTraNamNhuan(){
     let a = document.getElementById("textbox").value;
     let b = parseInt(a);
-    if (b%400==0 && b%100==0 || b%4==0){
-        alert("Năm đó là năm nhuận");}
-        else {alert("Năm đó không là năm nhuận");}
+    if (b%4==0){
+        if (b%100==0){
+            if (b%400==0){
+                alert("năm đó là năm nhuận");
+            }
+            else alert("năm đó không là năm nhuận");
+        }
+        else alert ("năm đó là năm nhuận");
     }
+    else alert ("năm đó không là năm nhuận");
+}
